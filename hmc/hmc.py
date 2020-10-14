1d5a0e3ec01f30851ff0169944a5f6c80f659408
Ariel Rokem <arokem@gmail.com>
2020-10-14 15:42:45 -0700

    Still very rough.

--- hmc/hmc.py
+++ hmc/hmc.py
@@ -2,31 +2,29 @@ import numpy as np
 
 from .registration import (affine_registration, register_series,
                            read_img_arr_or_path)
-from dipy.reconst.sfm import SparseFascicleModel
-from sklearn.model_selection import LeaveOneOut
+from dipy.reconst.sfm import SparseFascicleModel, sfm_design_matrix
 import dipy.core.gradients as dpg
 import collections
-from fracridge import FracRidgeRegressor, fracridge
-from dipy.core.optimize import SKLearnLinearSolver
+from fracridge import FracRidgeRegressor
+import nibabel as nib
+
+
+def prep_data(gtab, data, mask=None):
+    if mask is None:
+        mask = np.ones(data.shape[:3]).astype(bool)
+    b0 = np.mean(data[mask][:, gtab.b0s_mask], -1)
+    dwi = data[mask][:, ~gtab.b0s_mask] / b0[np.newaxis].T
+    return dwi.T
 
 
 def prep_sfm(gtab, data, mask=None):
-    if mask is None:
-        mask = np.ones(data.shape[:3])
+    y = prep_data(gtab, data, mask)
     sfm = SparseFascicleModel(gtab)
     X = sfm.design_matrix
-    b0 = np.mean(data[mask][:, gtab.b0s_mask], -1)
-    dwi = data[mask][:, ~gtab.b0s_mask] / b0[np.newaxis].T
-    y = dwi.T
     return X, y
 
 
-def fit_sfm(clf, X, y):
-    clf.fit(X, y)
-    return X, y, clf
-
-
-def hmc(img, gtab, mask=None, b0_ref=0):
+def hmc(data, gtab, mask=None, b0_ref=0, affine=None):
     data, affine = read_img_arr_or_path(data, affine=affine)
     if isinstance(gtab, collections.Sequence):
         gtab = dpg.gradient_table(*gtab)
@@ -35,8 +33,7 @@ def hmc(img, gtab, mask=None, b0_ref=0):
     # b0 volumes (first, per default):
     if np.sum(gtab.b0s_mask) > 1:
         b0_img = nib.Nifti1Image(data[..., gtab.b0s_mask], affine)
-        trans_b0, b0_affines = register_series(b0_img, ref=b0_ref,
-                                               pipeline=pipeline)
+        trans_b0, b0_affines = register_series(b0_img, ref=b0_ref)
         ref_data = np.mean(trans_b0, -1)
     else:
         # There's only one b0 and we register everything to it
@@ -51,14 +48,21 @@ def hmc(img, gtab, mask=None, b0_ref=0):
     for loo in range(moving_data.shape[-1]):
         loo_idxer = np.ones(moving_data.shape[-1]).astype(bool)
         loo_idxer[loo] = False
-        in_data = np.concatenate([ref_data, moving_data[..., loo_idxer]], -1)
-        in_gtab = dpg.gradient_table(
-            np.concatenate([0, moving_bvals[loo_idxer]]),
-            np.concatenate([0, moving_bvecs[loo_idxer]]))
-        clf = fit_sfm(in_gtab, in_data, clf)
+        in_data = np.concatenate([ref_data[..., np.newaxis], moving_data[..., loo_idxer]], -1)
 
+        in_gtab = dpg.gradient_table(
+            np.concatenate([np.array([0]), moving_bvals[loo_idxer]]),
+            np.concatenate([np.array([[0, 0, 0]]), moving_bvecs[loo_idxer]]))
+
+        X, y = prep_sfm(in_gtab, in_data, mask=None)
+        valid_targets = np.where(np.isfinite(y))[1]
+        clf.fit(X, y[:, valid_targets])
         out_data = moving_data[..., ~loo_idxer]
         out_gtab = dpg.gradient_table(moving_bvals[~loo_idxer],
                                       moving_bvecs[~loo_idxer])
-        pred_X = out_gtab
-        out_pred = clf.predict()
+
+
+        pred_X = sfm_design_matrix(out_gtab,
+                                   clf.sphere,
+                                   clf.response, mode='signal')
+        out_pred = clf.predict(pred_X)
--- hmc/test_hmc.py
+++ hmc/test_hmc.py
@@ -1,15 +1,17 @@
-from .hmc import hmc, fit_sfm, prep_sfm
+from .hmc import hmc, prep_sfm
 from dipy.data import get_fnames
 from dipy.core.gradients import gradient_table
 import nibabel as nib
 import numpy as np
-from fracridge import FracRidgeRegressor, fracridge
+from fracridge import FracRidgeRegressor
 
-# def test_hmc():
-#     fdata, fbvals, fbvecs = get_fnames("stanford_hardi")
-#     gtab = gradient_table(fbvals, fbvecs, b0_threshold=0)
-#     img = nib.load(fdata).get_fdata()
-#     hmc(img, gtab)
+
+def test_hmc():
+    fdata, fbvals, fbvecs = get_fnames("stanford_hardi")
+    gtab = gradient_table(fbvals, fbvecs, b0_threshold=0)
+    img = nib.load(fdata)
+    data = img.get_fdata()
+    hmc(data, gtab, affine=img.affine)
 
 
 def test_fracridge_sfm():
@@ -21,6 +23,6 @@ def test_fracridge_sfm():
     mask[40:50, 40:50, 40:50] = True
     X, y = prep_sfm(gtab, data, mask=mask)
     clf = FracRidgeRegressor()
-    X, y, clf = fit_sfm(clf, X, y)
+    clf.fit(X, y)
     # Shape is n_regressors, n_alphas, n_voxels:
     assert clf.coef_.shape == (362, 10, 1000)
